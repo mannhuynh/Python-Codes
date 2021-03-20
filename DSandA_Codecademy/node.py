992e493046403b9aa96a8c90795f4b1de4417b1a
Vuong Huynh <mannhuynh@gmail.com>
2021-03-20 11:12:37 -0500

    add queue.py

--- DSandA_Codecademy/node.py
+++ DSandA_Codecademy/node.py
@@ -1,13 +1,13 @@
 class Node:
-  def __init__(self, value, link_node=None):
+  def __init__(self, value, next_node=None):
     self.value = value
-    self.link_node = link_node
+    self.next_node = next_node
     
-  def set_link_node(self, link_node):
-    self.link_node = link_node
+  def set_next_node(self, next_node):
+    self.next_node = next_node
     
-  def get_link_node(self):
-    return self.link_node
+  def get_next_node(self):
+    return self.next_node
   
   def get_value(self):
     return self.value
@@ -17,11 +17,11 @@ yacko = Node("likes to yak")
 wacko = Node("has a penchant for hoarding snacks")
 dot = Node("enjoys spending time in movie lots")
 
-yacko.set_link_node(dot)
-dot.set_link_node(wacko)
+yacko.set_next_node(dot)
+dot.set_next_node(wacko)
 
-dots_data = yacko.get_link_node().get_value()
-wackos_data = dot.get_link_node().get_value()
+dots_data = yacko.get_next_node().get_value()
+wackos_data = dot.get_next_node().get_value()
 
 print(dots_data)
 print(wackos_data)
