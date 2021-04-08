e8202612d784bc540eac0c3c40d830e331e8d296
Vuong Huynh <mannhuynh@gmail.com>
2021-04-08 15:42:45 -0500

    Add get_weather() to geopoint.py

--- OOP/APP_1/geopoint.py
+++ OOP/APP_1/geopoint.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from pytz import timezone
 from timezonefinder import TimezoneFinder
+from sunnyday import Weather
 
 
 class GeoPoint:
@@ -24,7 +25,15 @@ class GeoPoint:
         time_now = datetime.now(timezone(timezone_string))
         return time_now
 
+    def get_weather(self):
+        """
+        Weather class need apikey from https://openweathermap.org.
+        """
+        weather = Weather(apikey="26631f0f41b95fb9f5ac0df9a8f43c92", lat=self.latitude, lon=self.longitude)
+        return weather.next_12h_simplified()
+
 
 tokyo = GeoPoint(latitude=35.7, longitude=139.7)
 print(tokyo.closest_parallel())
 print(tokyo.get_time())
+print(tokyo.get_weather())
