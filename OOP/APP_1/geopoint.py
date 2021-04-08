79d71d1b2905e4f49cf003c70d4c798db91ff060
Vuong Huynh <mannhuynh@gmail.com>
2021-04-08 16:16:51 -0500

    Add class method and class attributes to GeoPoint

--- OOP/APP_1/geopoint.py
+++ OOP/APP_1/geopoint.py
@@ -2,9 +2,15 @@ from datetime import datetime
 from pytz import timezone
 from timezonefinder import TimezoneFinder
 from sunnyday import Weather
+from random import uniform
 
 
 class GeoPoint:
+
+    # Below are class attributes, accessed directly from class, not instance
+    # Using: GeoPoint.latitude_range, and GeoPoint.longitude_range
+    latitude_range = (-90, 90)
+    longitude_range = (-180, 180)
 
     def __init__(self, latitude, longitude):
         self.latitude = latitude
@@ -29,11 +35,21 @@ class GeoPoint:
         """
         Weather class need apikey from https://openweathermap.org.
         """
-        weather = Weather(apikey="26631f0f41b95fb9f5ac0df9a8f43c92", lat=self.latitude, lon=self.longitude)
+        weather = Weather(apikey="26631f0f41b95fb9f5ac0df9a8f43c92",
+                          lat=self.latitude,
+                          lon=self.longitude)
         return weather.next_12h_simplified()
+
+    @classmethod
+    def random(cls):
+        """
+        This class method generate the random latitude and longitude using: GeoPoint.random()
+        """
+        return cls(latitude=uniform(-90, 90), longitude=uniform(-180, 180))
 
 
 tokyo = GeoPoint(latitude=35.7, longitude=139.7)
 print(tokyo.closest_parallel())
 print(tokyo.get_time())
 print(tokyo.get_weather())
+print(GeoPoint.latitude_range, GeoPoint.longitude_range)
